7240469fbcb1fb58cadc3e9ec1d61023b1220c4c
BuildTools <tranquangduc000@gmail.com>
2020-03-15 07:07:46 +0700

    ccc
    upload menu header

--- Giao Dien JV5/Global-JS.js
+++ Giao Dien JV5/Global-JS.js
@@ -56,3 +56,66 @@ function pingMess(message, miniSecond) {
         }, miniSecond);
     }
 }
+
+////////////////////////////////////////////////////////////////
+function getCookie(cookieName) {
+    let ca = decodeURIComponent(document.cookie).split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookieName) === 0) {
+            return c.substring(cookieName.length + 1, c.length);
+        }
+    }
+    return undefined;
+}
+function setUsername(username) {
+    if (username === undefined || username === null || username === "null" || username === "none" || username === "unset" ) {
+        document.querySelector("body > header.w3-row > div.w3-right.right.w3-col > div.titleUserName > label").innerHTML = getCookie("username");
+    } else {
+        document.cookie = "username=" + username;
+        document.querySelector("body > header.w3-row > div.w3-right.right.w3-col > div.titleUserName > label").innerHTML = username;
+    }
+}
+setUsername();
+
+document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm1").addEventListener("click", function () {
+    let btn = document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm1");
+    let menu = document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > div.menu");
+    if (btn.classList.contains("accept")) {
+        btn.classList.remove("accept");
+        menu.style.display = "none";
+    } else {
+        btn.classList.add("accept");
+        menu.style.display = "block";
+
+    }
+});
+
+document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm2").addEventListener("mouseover", function () {
+    document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm2 > label").style.display = "flex";
+});
+document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm2 > label").addEventListener("mouseout", function () {
+    document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm2 > label").style.display = "none";
+});
+
+function changeLang(lang) {
+    document.cookie = "org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=" + lang;
+    location.reload();
+}
+ 
+function checkLogin() {
+    if (getCookie("checklogin") === "false") {
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.titleLogin").style.display = "block";
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.titleUserName").style.display = "none";
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm1").style.display = "none";
+    } else {
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.titleLogin").style.display = "none";
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.titleUserName").style.display = "block";
+        document.querySelector("body > header > div.w3-right.right.w3-col > div.hMenu > a.bm1").style.display = "flex";
+    }
+}
+
+checkLogin();
